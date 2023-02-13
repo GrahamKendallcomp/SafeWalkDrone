50c0ff707d744931544a7b172401650202bfcf1f
Graham Kendall <grahamkendall@cmail.carleton.ca>
2023-02-13 10:15:14 -0500

    Comments for bailey

--- ground_station/main.py
+++ ground_station/main.py
@@ -5,6 +5,8 @@ def main():
     if __name__ == "__main__":
         sitl, drone = groundStation.init()
         while(True):
+            #put constant yolov5 detection here
+            #use commands.py  drone commands for drone control
             pass
 
         groundStation.shutdown(sitl, drone)
