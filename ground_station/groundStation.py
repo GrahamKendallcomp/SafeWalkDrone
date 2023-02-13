06db5bd0accd79ec02b9abd6d1377d7d06ab4693
Graham Kendall <grahamkendall@cmail.carleton.ca>
2023-02-13 10:07:40 -0500

    Added automatic entire groundstation setup

--- ground_station/groundStation.py
+++ ground_station/groundStation.py
@@ -1,29 +1,38 @@
-print( "Start simulator (SITL)") 
 import dronekit_sitl
-sitl = dronekit_sitl.start_default()
-connection_string = sitl.connection_string()
-#connection_string = "tcp:10.242.25.163:1204" #get rpi zerotier ip and port
-connection_string = "tcp:10.242.215.212:14550" #get rpi zerotier ip and port
-#connection_string = "tcp:127.0.0.1:12000"
-# Import DroneKit-Python
 from dronekit import connect, VehicleMode
+from paramiko import SSHClient
 
-# Connect to the Vehicle.
-print(("Connecting to vehicle on: %s" % (connection_string,))) 
-vehicle = connect(connection_string, wait_ready=True)
 
-# Get some vehicle attributes (state)
-print( "Get some vehicle attribute values:") 
-print( " GPS: %s" % vehicle.gps_0) 
-print( " Battery: %s" % vehicle.battery) 
-print( " Last Heartbeat: %s" % vehicle.last_heartbeat) 
-print( " Is Armable?: %s" % vehicle.is_armable) 
-print( " System status: %s" % vehicle.system_status.state) 
-print( " Mode: %s" % vehicle.mode.name )   # settable
+def init():
+    print("Creating ssh connection to RPi")
+    client = SSHClient()
+    client.connect('10.242.215.212', username='stephen', password='4thyearproject2022!')
+    
+    print("Setting up MAVProxy")
+    stdin, stdout, stderr = client.exec_command('mavproxy.py --out=tcpin:0.0.0.0:14550')
+    print(stdin)
+    print(stdout)
+    print(stderr)
+    print("Closing ssh connection")
+    client.close()
 
-# Close vehicle object before exiting script
-vehicle.close()
+    print( "Start simulator (SITL)") 
+    sitl = dronekit_sitl.start_default()
 
-# Shut down simulator
-sitl.stop()
-print("Completed")
+    connection_string = "tcp:10.242.215.212:14550"
+    print(("Connecting to vehicle on: %s" % (connection_string,))) 
+    vehicle = connect(connection_string, wait_ready=True)
+    print( "Get some vehicle attribute values:") 
+    print( " GPS: %s" % vehicle.gps_0) 
+    print( " Battery: %s" % vehicle.battery) 
+    print( " Last Heartbeat: %s" % vehicle.last_heartbeat) 
+    print( " Is Armable?: %s" % vehicle.is_armable) 
+    print( " System status: %s" % vehicle.system_status.state) 
+    print( " Mode: %s" % vehicle.mode.name ) 
+
+    return sitl, vehicle
+
+def shutdown(sitl, vehicle):
+    vehicle.close()
+    # Shut down simulator
+    sitl.stop()
---
+++ ground_station/main.py
@@ -0,0 +1,10 @@
+import groundStation
+import commands 
+
+def main():
+    if __name__ == "__main__":
+        sitl, drone = groundStation.init()
+        while(True):
+            pass
+
+        groundStation.shutdown(sitl, drone)
