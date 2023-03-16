e52b737b1b5f6785294e543a831b3bce5d256d04
Bailey Lyster <71287969+baileylyster@users.noreply.github.com>
2023-03-16 14:36:48 -0400

    ++

--- ground_station/makeInstruction.py
+++ ground_station/makeInstruction.py
@@ -13,95 +13,85 @@ psi = 1
 centreScreen = (piCameraResolution[0]/2,piCameraResolution[1]/2)
 
 class makeInstruction:
+     
+    def __init__(self) -> None:
+        pass
 
-    def getBboxCentre(bbox):
-            return (((bbox[2]-bbox[0])/2 + bbox[0]), (((bbox[3]-bbox[1])/2)+bbox[1]))
+def getBboxCentre(bbox):
+        return (((bbox[2]-bbox[0])/2 + bbox[0]), (((bbox[3]-bbox[1])/2)+bbox[1]))
 
-    def checkPixelHeight(bbox):
-            height = bbox[3] - bbox[1]
-            if((height > 250 ) & (height < 180)):
-                return (True,height)
-            else:
-                return (False,height)
-            
-    #Parallax Correction for Height
-    def pHeightRealHeight(pixels):
-        theta = cameraAngle
-        realPHeight =  pixels * 1/(m.cos(theta) / (1 - m.sin(theta)))
-        return realPHeight
-
-
-    #Very rough estimate, Based on a 1.8m tall person
-    def pixelHeightDistanceAway(pixels):
-        return 3*pixels + 6
-
-
-
-
-    #Returns Distance to the Drone from the subject
-    def distanceToDrone(self,height):
-        return self.pHeightRealHeight(height) * m.tan(psi)
-
-
-    #Vector from the drone to the subject in meters
-    #mavLink uses x forward, y to the right, z ascend.
-    #Should be in 'meters' approximation
-    def vectorInstruction(self,bbox):
-        y,z =  ((self.getBboxCentre(bbox) - centreScreen))
-        x = self.distanceToDrone(self.pHeightRealHeight(self.checkPixelHeight(bbox)[1]))
-        return (x,y,z)
-
-
-        
-
-    def isCentre(self,bbox,centreScreen):
-        x,y,x2,y2 = True, True,0,0
-        bboxCentre = self.getBboxCentre(bbox)
-        ## is the BBox Centred? or Close enough to the centre for noise?
-        if((bboxCentre[0],bboxCentre[1] == centreScreen[0],centreScreen[1])):
-            return x,y,x2,y2
-        if(m.abs(bboxCentre[0] - centreScreen[0]) < 10) & ((m.abs(bboxCentre[1] - centreScreen[1])) ):
-            return x,y,x2,y2
-        #if its too far from the centre
+def checkPixelHeight(bbox):
+        height = bbox[3] - bbox[1]
+        if((height > 250 ) & (height < 180)):
+            return (True,height)
         else:
-            if (m.abs(bboxCentre[0] - centreScreen[0]) > 10):
-                x = False
-            if (m.abs(bboxCentre[1] - centreScreen[1]) > 10):
-                y = True
-            return x,y
+            return (False,height)
+        
+#Parallax Correction for Height
+def pHeightRealHeight(pixels):
+    theta = cameraAngle
+    realPHeight =  pixels * 1/(m.cos(theta) / (1 - m.sin(theta)))
+    return realPHeight
 
 
+#Very rough estimate, Based on a 1.8m tall person
+def pixelHeightDistanceAway(pixels):
+    
+    return 3*(pixels/piCameraResolution[1]) + 6
+
+
+def pixelLateralDistanceEstimation(pixels, lateralDistance):
+    return (6 * (pixels / piCameraResolution ) /lateralDistance) 
+    
+    
+    
+
+
+#Returns Distance to the Drone from the subject
+def distanceToDrone(height):
+    return pHeightRealHeight(height) * m.tan(psi)
+
+
+#Vector from the drone to the subject in meters
+#mavLink uses x forward, y to the right, z ascend.
+#Should be in 'meters' approximation
+def vectorInstruction(bbox):
+    y = getBboxCentre(bbox)[0]-centreScreen[0]
+    z = y = getBboxCentre(bbox)[1]-centreScreen[1]
+    x = distanceToDrone(pHeightRealHeight(checkPixelHeight(bbox)[1]))
+    return (x,y,z)
 
 
     
 
-    def __init__(self,
-                 frame,
-                 bbox,
-                 id,
-                 lastFrame,
-                 newInstruction
+def isCentre(bbox,centreScreen):
+    x,y,x2,y2 = True, True,0,0
+    bboxCentre = getBboxCentre(bbox)
+    ## is the BBox Centred? or Close enough to the centre for noise?
+    if((bboxCentre[0],bboxCentre[1] == centreScreen[0],centreScreen[1])):
+        return x,y,x2,y2
+    if(m.abs(bboxCentre[0] - centreScreen[0]) < 10) & ((m.abs(bboxCentre[1] - centreScreen[1])) ):
+        return x,y,x2,y2
+    #if its too far from the centre
+    else:
+        if (m.abs(bboxCentre[0] - centreScreen[0]) > 10):
+            x = False
+        if (m.abs(bboxCentre[1] - centreScreen[1]) > 10):
+            y = True
+        return x,y
+    
+
+def dataForDisplay(bbox):
+    x,y,z = vectorInstruction(bbox)
+    x = pixelHeightDistanceAway(checkPixelHeight(bbox)[1])
+    y,z = pixelLateralDistanceEstimation(y,x),pixelLateralDistanceEstimation(z,x)
+    return x,y,z
+    
+    
 
 
-    ):
-        self.frame = 0
-        self.lastFrame = []
 
-    def update(self,bbox):
-        self.bbox = bbox
-        self.frame += 1
-        self.id = bbox[4]
-        self.lastFrame.append((((bbox[2]-bbox[0])/2 + bbox[0]), (((bbox[3]-bbox[1])/2)+bbox[1])))
-        self.newInstruction = self.vectorInstruction(self.bbox)
-
-
-
-
-
-                 
-
-
-             
+            
             
 
         
--- main.py
+++ main.py
@@ -5,45 +5,109 @@ from pathlib import Path
 import multiprocessing as m
 import socket
 import pickle
+import threading as t
+import tkinter as tk
+import time
 
 from ground_station.groundStation import groundStation
-from ground_station.makeInstruction import makeInstruction
+from ground_station.makeInstruction import makeInstruction as instruct
 from ground_station.commands import *
-
+import queue
 
 
 
 
 
 def main():
+    global bboxlist 
     bboxlist = []
-    
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(('localhost', 8888))
     yolo = track.trackclass(bboxlist)
     yoloprocess = m.Process(target = yolo.run)
-    
+    tracking_gui = t.Thread(target = makeGUI)
     #sitl, drone = groundStation.init()
     missionDone = False
     altitude = 2
     yoloprocess.start()
+    tracking_gui.start()
+    
     #commands.arm_and_takeoff(drone, altitude)
-
     while(True):
         data = sock.recvfrom(1024)
-        bboxlist = pickle.loads(data[0])
-        
+        output = pickle.loads(data[0])
+        bboxlist = output
+        #print(bboxlist)
+        #try:
+        ##        q.queue.clear()
+            #    for items in bboxlist:
+          #         q.put(items)
+           #         #print(bboxlist)
+        #except:
+          #  print()
         #use commands.py  drone commands for drone control
         #pass
         if (missionDone):
             break
-    sock.close()
     #groundStation.shutdown(sitl, drone)
     #Yolov5Thread.exit()
+
+def makeGUI():
+    # create the GUI window
+    root = tk.Tk()
+    root.title("Drone Instructions")
+
+    # create two instances of the RealTimeDisplay widget
+    display1 = RealTimeInstruct(root)
+    display1.pack(side='left', padx=200, pady=200)
+    #display2 = RealTimeInstruct(root)
+    #display2.pack(side='left', padx=200, pady=200)
+
+    # start the GUI event loop
+    root.mainloop()
+
+
+
+
+class RealTimeInstruct(tk.Frame):
+    def __init__(self, parent):
+        #self.queue = q
+        #time.sleep(6000)
+        super().__init__(parent)
+        self.label = tk.Label(self, font=('Courier', 35))
+        self.label.pack()
+        self.update_Contents()
+        
+        
+
+    def update_Contents(self):
+        string = self.parseBboxList()
+        self.label.configure(text=string)
+        self.after(500, self.update_Contents)
+
+    def parseBboxList(self)->str:
+        #bboxlist = bboxlist
+        try: 
+            if(bboxlist != []):
+                if (bboxlist[0] != []):
+                    #print(bboxlist)
+                    print(bboxlist[0])
+                    temp = instruct.dataForDisplay(bboxlist[0])
+                    print(temp)
+                    word = "ID: " + str(bboxlist[0][4]) + "move forward " + str(round(temp[0],2)) +'ft' + " move laterally " +  str(round(temp[1],2)) +'ft ' + "move vertically" + str(round(temp[2],2)) + 'ft'
+                    #print(word)
+                    return word 
+                else:
+                    return 'potato'
+        except:
+            pass
+
+
+    
+
+
     
 
 if __name__ == "__main__":
     main()
     
-
-
